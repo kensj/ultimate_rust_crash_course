42019dc80eca8caccf2935b9dbde798f2bf79917
Kenji Japra <kenji.japra@gmail.com>
2022-06-04 09:00:29 -0400

    update

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,6 +1,39 @@
 use hello::{english, russian, spanish};
 
+trait Movement {
+    fn move(&self) ->{
+        println!("I'm moving");
+    }
+}
+trait Fly {
+
+}
+impl Movement for Fly;
+trait Run {
+
+}
+imply Movement for Run;
+
+struct Goose {
+    beak_length: i8,
+}
+impl Fly for Goose;
+impl Run for Goose;
+
+struct Pegasus {
+    wing_length: i8,
+}
+impl Fly for Pegasus;
+impl Run for Pegasus;
+
+struct Horse {
+    mane_length: i8,
+}
+impl Run for Horse;
+
 fn main() {
+
+
     english::greet();
     spanish::greet();
     russian::greet();
